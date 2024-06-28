1e147fa52147b04ec48554d991f8fcff6d337411
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-06-28 11:45:34 +0800

    Pass test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, remove) {
+  let newArr = arr;
+  for(i = 0; i < arr.length; i++) {
+    if(arr[i] === remove) {
+      newArr.splice(i, 1);
+    }
+  }
+
+  return newArr;
 };
 
 // Do not edit below this line
