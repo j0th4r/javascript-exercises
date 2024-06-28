5624bbabcfd071854a06ba296631306e94e0e2aa
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-06-28 18:21:53 +0800

    finish exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(augend, addend) {
+  let sum = 0;
+  
+  for(let i = 0; i < arguments.length; i++) {
+    if(arguments[i] < 0 || typeof arguments[i] !== "number") {
+      return "ERROR";
+    }
+  }
 
+  let temp = addend;
+  if(augend > addend) {
+    addend = augend;
+    augend = temp;
+  }
+
+  for(let i = augend; i <= addend; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
