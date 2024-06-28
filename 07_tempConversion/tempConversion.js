5b009e72578f54c47798979e8422d787ad4d9c56
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-06-28 19:01:45 +0800

    finish convert to celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,8 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  const result = (x - 32) * (5/9);
+  const rounded = Math.round(result * 10) / 10;
+
+  return rounded;
 };
 
 const convertToFahrenheit = function() {
