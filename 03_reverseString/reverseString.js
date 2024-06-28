4e2f9e058481e67698e9c22fe2159386b57c7ed6
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-06-28 11:27:34 +0800

    finish exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const char = string.split("");
+  let result = ""
+  for(i = string.length - 1; i >= 0; i--) {
+    result += char[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
