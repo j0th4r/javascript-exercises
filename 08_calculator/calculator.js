822698f850142e4ca7a43c1604e38ef666d6c2f3
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-07-19 22:34:22 +0800

    Implement factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -24,8 +24,11 @@ const power = function(base, exponent) {
 };
 
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0 || num == 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
