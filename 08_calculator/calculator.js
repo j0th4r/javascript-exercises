60897fa36b65097876cc33ee2e8bb11255226866
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-07-19 22:24:57 +0800

    Implement power function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -19,9 +19,10 @@ const multiply = function(array) {
   });
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
+
 
 const factorial = function() {
 	
