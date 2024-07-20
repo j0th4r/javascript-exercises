e42a277dcbd7fca79cedf0715f992e757d25d448
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-07-20 19:59:52 +0800

    Finish palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const str = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g, "").toLowerCase()
+  return str
+          .split("")
+          .reverse()
+          .join("") === str ? true : false;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
