5b491a1786c75de2ec730d129c7ab7bad8f36b62
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-07-20 21:38:12 +0800

    Finish all exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  const date = new Date();
+  const year = date.getFullYear();
+  array.map(person => person.yearOfDeath === undefined ? person.yearOfDeath = year : person.yearOfDeath);
+  return array.sort((a, b) => a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? -1 : 1)[0];
 };
 
 // Do not edit below this line
